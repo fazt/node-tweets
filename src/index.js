5ec712dbbd66d843732be408c9b4b2fe5bfdd42f
fazt <me@faztweb.com>
2017-11-19 23:39:20 -0500

    i forgot to save the file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ const twitter = require('twitter');
 
 // use varible enviroments
 const client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: ''.
-  access_token_secret: ''
+  consumer_key: 'Cn8JWHZHbS45onO8X2z72g1Nx',
+  consumer_secret: '5nCme4YK1aFMFzXbivlzR5PVbjhUTYMtdQpes2yQUN5P6qLRXs',
+  access_token_key: '3407656973-7GHlOHWNHAwky9WlvekumkKMvk1INmxZgBEZAuL'.
+  access_token_secret: 'OJSQ5ajAUiLkhWrQgX9u6RP2st1VLb1abhKYfILEwKL2x'
+});
+
+client.post('/statuses/update', {
+  status: 'A tweet from an App with Nodejs!'
+}).then(tweet => {
+  console.log(tweet);
+}).catch(err => {
+
 });
